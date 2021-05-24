4d3921be47a29a1133206378697672b89a5a440b
spiesko <spiesko@sii.pl>
2021-05-24 11:57:16 +0200

    BasicTableTest.java update

--- src/test/java/BasicTableTest.java
+++ src/test/java/BasicTableTest.java
@@ -14,11 +14,26 @@ public class BasicTableTest extends TestBase{
 
         for (WebElement mountain : allMountains) {
             List<WebElement> allColumns = mountain.findElements(By.cssSelector("td"));
-
             if (Integer.parseInt(allColumns.get(3).getText()) > 4000) {
                 System.out.println(allColumns.get(0).getText());
             }
+        }
+    }
 
+    @Test
+    public void shouldPrintRankPeakMountainRangeInSwitzerlandHigherThan4000() {
+        getDriver().get("https://seleniumui.moderntester.pl/table.php");
+
+        List<WebElement> allMountains = getDriver().findElements(By.cssSelector("ybody tr"));
+
+        for (WebElement mountain : allMountains) {
+            List<WebElement> allColumns = mountain.findElements(By.cssSelector("td"));
+            if (Integer.parseInt(allColumns.get(3).getText()) > 4000) {
+                System.out.println(allColumns.get(0).getText());
+                System.out.println(allColumns.get(1).getText());
+                System.out.println(allColumns.get(2).getText());
+
+            }
         }
 
 
