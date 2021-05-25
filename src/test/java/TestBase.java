a94ac85cc6ba5f5814df6ca4ea904f2b864400e3
spiesko <spiesko@sii.pl>
2021-05-25 13:58:18 +0200

    update TestBase.java

--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -1,36 +1,28 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class TestBase {
 
-    private static Logger logger = LoggerFactory.getLogger("TestBase.java");
-
-    private static WebDriver driver;
+    private WebDriver driver;
 
     public WebDriver getDriver() {
         return driver;
     }
 
-    @BeforeAll
-    static void setUp() {
+    @BeforeEach
+    public void setUp(){
         ChromeOptions options = new ChromeOptions();
         WebDriverManager.chromedriver().setup();
         options.addArguments("start-maximized");
         driver = new ChromeDriver(options);
-        logger.info(">>>>> Starting test <<<<<");
     }
 
-    @AfterAll
-    static void tearDown() {
+    @AfterEach
+    public void cleanUp(){
         driver.quit();
-        logger.info("Driver has been closed properly");
-        logger.info(">>>>> Finished test <<<<<");
     }
-
 }
